3ab48e248695f126ff86cd209e2dec6bdb9dc23a
Prandtl <prandtl211@gmail.com>
2015-04-08 17:11:09 +0500

    visualisator added

---
+++ Forest/Visualisator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forest
+{
+	interface IVisualisator
+	{
+		void OnNewState(IForest map);
+	}
+	class Visualisator:IVisualisator
+	{
+		public void OnNewState(IForest forest)
+		{
+			var size = forest.GetForestSize();
+
+			var width = size.X;
+
+			var rows = forest.G
+		}
+
+		private Dictionary<string, char> artistsView = new Dictionary<string, char>
+		{
+			{"0", ' '},
+			{"1",'█'},
+			{"L",'♥'},
+			{"K",'X'}
+		};
+	}
+}
