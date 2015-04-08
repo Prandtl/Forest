25cef26feb3cd4a7888e8d8c88e53bf1c8758f4c
Prandtl <prandtl211@gmail.com>
2015-04-08 17:21:44 +0500

    visuslisator completed. not tested.

--- Forest/Visualisator.cs
+++ Forest/Visualisator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,7 @@ namespace Forest
 	{
 		void OnNewState(IForest map);
 	}
-	class Visualisator:IVisualisator
+	class Visualisator : IVisualisator
 	{
 		public void OnNewState(IForest forest)
 		{
@@ -18,7 +19,24 @@ namespace Forest
 
 			var width = size.X;
 
-			var rows = forest.G
+			var rows = forest.GetPoints();
+			var enumerator = rows.GetEnumerator();
+
+
+			var j = 0;
+			while (enumerator.MoveNext())
+			{
+				if (j == width)
+				{
+					j = 0;
+					Console.Write('\n');
+				}
+				var e = enumerator.Current;
+				char c;
+				if (!artistsView.TryGetValue(e, out c))
+					c = e[0];
+				Console.Write(c);
+			}
 		}
 
 		private Dictionary<string, char> artistsView = new Dictionary<string, char>
